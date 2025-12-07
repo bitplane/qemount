3628a9c9ad971ef6097c257b0993b400abd2b611
Gareth Davidson <gaz@bitplane.net>
2025-12-07 09:01:02 +0000

    dispatch and clean exit

--- guests/netbsd/10.0/ramdisk/init.sh
+++ guests/netbsd/10.0/ramdisk/init.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-# Shell mode - just drop to interactive shell
-echo "Entering shell mode"
-exec /bin/sh
+echo "[SH] Shell mode"
+echo "[SH] MOUNT_POINT=$MOUNT_POINT"
+echo "[SH] MOUNT_SUCCESS=$MOUNT_SUCCESS"
+echo ""
+
+if [ "$MOUNT_SUCCESS" = "1" ]; then
+    echo "Image mounted at $MOUNT_POINT"
+    echo "Type 'ls $MOUNT_POINT' to explore"
+else
+    echo "No image mounted"
+fi
+
+echo ""
+echo "Type 'exit' to shutdown"
+echo ""
+
+exec /bin/sh -i
