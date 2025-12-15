bcc2eeb7a4722765e63f7f098131ed4c7c9fd309
Gareth Davidson <gaz@bitplane.net>
2025-12-15 22:18:48 +0000

    add more filesystems

---
+++ tests/data/fs/hfsplus/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+OUTPUT_PATH="$1"
+BUILD=/host/build
+
+# Copy ext2 source and remove lost+found
+# TODO: use tarfs once supported, to avoid this ext2 workaround
+cp "$BUILD/tests/data/fs/basic.ext2" /tmp/source.ext2
+debugfs -w -R "rmdir lost+found" /tmp/source.ext2 2>/dev/null || true
+SRC_IMG="/tmp/source.ext2"
+
+KERNEL="$BUILD/guests/linux/6.17/x86_64/kernel"
+BOOT_IMG="$BUILD/guests/linux/rootfs/x86_64/boot.img"
+RUN_SCRIPT="$BUILD/common/run/qemu-linux/run-linux.sh"
+
+truncate -s 16M /tmp/output.hfsplus
+mkfs.hfsplus /tmp/output.hfsplus
+
+"$RUN_SCRIPT" x86_64 "$KERNEL" "$BOOT_IMG" \
+    -i "$SRC_IMG" -i /tmp/output.hfsplus -m duplicate
+
+mkdir -p "$(dirname "$BUILD/$OUTPUT_PATH")"
+cp /tmp/output.hfsplus "$BUILD/$OUTPUT_PATH"
