12e6e2266b299db42c9e313cf79a2c700b436364
Gareth Davidson <gaz@bitplane.net>
2026-01-11 13:46:40 +0000

    proper static build properly all rproper like

--- src/qemount_build/bin/reiserfsprogs/build.sh
+++ src/qemount_build/bin/reiserfsprogs/build.sh
@@ -5,8 +5,11 @@ cd /work
 tar xf /host/build/sources/reiserfsprogs-3.6.27.tar.xz
 cd reiserfsprogs-3.6.27
 
-./configure CFLAGS="-D_GNU_SOURCE" LDFLAGS="-static"
-make -j${JOBS}
+./configure --disable-shared CFLAGS="-D_GNU_SOURCE -static" LDFLAGS="-static"
+make -j${JOBS} -C include
+make -j${JOBS} -C lib
+make -j${JOBS} -C reiserfscore
+make -j${JOBS} -C mkreiserfs LDFLAGS="-static -all-static"
 
 mkdir -p /host/build/bin/linux-${HOST_ARCH}/reiserfsprogs
 cp mkreiserfs/mkreiserfs /host/build/bin/linux-${HOST_ARCH}/reiserfsprogs/mkfs.reiserfs
