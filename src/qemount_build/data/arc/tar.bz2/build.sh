b4eae174129658a148c1a0bb1232e68ebe4b8711
Gareth Davidson <gaz@bitplane.net>
2026-01-22 18:28:45 +0000

    fix tar busybox for non -J

--- src/qemount_build/data/arc/tar.bz2/build.sh
+++ src/qemount_build/data/arc/tar.bz2/build.sh
@@ -8,7 +8,7 @@ TAR_PATH="/host/build/tests/data/templates/${BASE_NAME}.tar"
 mkdir -p /tmp/template
 tar -xf "$TAR_PATH" -C /tmp/template
 
-tar -cjf /tmp/output.tar.bz2 -C /tmp/template .
+tar -cf - -C /tmp/template . | bzip2 > /tmp/output.tar.bz2
 
 mkdir -p "$(dirname "/host/build/$OUTPUT_PATH")"
 cp /tmp/output.tar.bz2 "/host/build/$OUTPUT_PATH"
--- src/qemount_build/data/arc/tar.xz/build.sh
+++ src/qemount_build/data/arc/tar.xz/build.sh
@@ -8,7 +8,7 @@ TAR_PATH="/host/build/tests/data/templates/${BASE_NAME}.tar"
 mkdir -p /tmp/template
 tar -xf "$TAR_PATH" -C /tmp/template
 
-tar -cJf /tmp/output.tar.xz -C /tmp/template .
+tar -cf - -C /tmp/template . | xz > /tmp/output.tar.xz
 
 mkdir -p "$(dirname "/host/build/$OUTPUT_PATH")"
 cp /tmp/output.tar.xz "/host/build/$OUTPUT_PATH"
