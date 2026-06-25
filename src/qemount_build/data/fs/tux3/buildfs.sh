5e1b76b2920a39c75131abf2923f2e2012a4fd4b
Gareth Davidson <gaz@bitplane.net>
2026-06-25 06:43:16 +0100

    tux3 set image size before filling

--- src/qemount_build/data/fs/tux3/buildfs.sh
+++ src/qemount_build/data/fs/tux3/buildfs.sh
@@ -4,4 +4,6 @@
 
 # Tux3 was never in mainline Linux so we can only create a
 # minimal formatted image for detection testing.
+# mkfs.tux3 will not size an empty file itself, so pre-size it first.
+truncate -s 10M "$2"
 mkfs.tux3 "$2"
