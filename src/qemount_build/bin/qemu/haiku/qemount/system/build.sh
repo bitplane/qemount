76785e68fc759166dd3bc956ea6d68d6fc218247
Gareth Davidson <gaz@bitplane.net>
2026-07-31 20:02:49 +0100

    punch hole in podman container for cache

--- src/qemount_build/bin/qemu/haiku/qemount/system/build.sh
+++ src/qemount_build/bin/qemu/haiku/qemount/system/build.sh
@@ -4,6 +4,17 @@ set -eu
 OUTPUT_DIR=/host/build/bin/qemu/${ARCH}-haiku/qemount/system
 OUTPUT_IMAGE=$OUTPUT_DIR/haiku.image
 BUILD_JOBS=${JOBS:-1}
+CACHE_DIR=/host/build/cache/haiku/${ARCH}/${HAIKU_REVISION}/qemount-v2
+
+mkdir -p "$CACHE_DIR"
+if [ ! -f "$CACHE_DIR/Jamfile" ]; then
+    cd "$CACHE_DIR"
+    /src/haiku/configure \
+        --cross-tools-prefix \
+        "/toolchains/cross-tools-${ARCH}/bin/${ARCH}-unknown-haiku-"
+fi
+
+cd "$CACHE_DIR"
 
 jam -q -j"$BUILD_JOBS" \
     "-sHAIKU_IMAGE_SIZE=$HAIKU_IMAGE_SIZE" \
@@ -11,4 +22,4 @@ jam -q -j"$BUILD_JOBS" \
     @qemount-raw
 
 mkdir -p "$OUTPUT_DIR"
-install -m 644 /src/haiku/generated/haiku-qemount.image "$OUTPUT_IMAGE"
+install -m 644 "$CACHE_DIR/haiku-qemount.image" "$OUTPUT_IMAGE"
